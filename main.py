dbbd96e36914f7ec9a4a1bdec2b6198c0a4d9b5c
Juergen Pfundt <juergen.pfundt@gmail.com>
2023-08-11 16:39:12 +0200

    define main function

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import reactivex as rx
 from pyftdi.ftdi import Ftdi
 
-ftdi = Ftdi()
-# list available ftdi devices
-# on macOS do a `ls -lta /dev/cu*` when the ftdi microcontroller is connected
-print(f"List Devices: {ftdi.list_devices()}")
-# open a dedicated ftdi device contained in the list of ftdi devices
-# URL Scheme
-# ftdi://[vendor][:[product][:serial|:bus:address|:index]]/interface
-ftdi.open_from_url("ftdi:///1")
 
-from DigitalizeVideo import DigitalizeVideo
-from Ft232hConnector import Ft232hConnector
+def main():
+    ftdi = Ftdi()
+    # list available ftdi devices
+    # on macOS do a `ls -lta /dev/cu*` when the ftdi microcontroller is connected
+    print(f"List Devices: {ftdi.list_devices()}")
+    # open a dedicated ftdi device contained in the list of ftdi devices
+    # URL Scheme
+    # ftdi://[vendor][:[product][:serial|:bus:address|:index]]/interface
+    ftdi.open_from_url("ftdi:///1")
 
-optoCouplerSignalSubject: rx.subject.subject.Subject = rx.subject.Subject()
+    from DigitalizeVideo import DigitalizeVideo
+    from Ft232hConnector import Ft232hConnector
 
-device_number = 0  # number of camera device
+    optoCouplerSignalSubject: rx.subject.subject.Subject = rx.subject.Subject()
 
-# create class instances
-ft232h = Ft232hConnector(optoCouplerSignalSubject)
-dv = DigitalizeVideo(device_number, optoCouplerSignalSubject)
+    device_number = 0  # number of camera device
 
-# create monitoring window
-dv.create_monitoring_window()
-# start recording
-# wait for signal(s) to take picture(s)
-ft232h.signal_input()
-# delete monitoring window
-dv.delete_monitoring_window()
-# release usb camera
-dv.release_camera()
-# disconnect FT232H
-ftdi.close()
+    # create class instances
+    ft232h = Ft232hConnector(optoCouplerSignalSubject)
+    dv = DigitalizeVideo(device_number, optoCouplerSignalSubject)
+
+    # create monitoring window
+    dv.create_monitoring_window()
+    # start recording
+    # wait for signal(s) to take picture(s)
+    ft232h.signal_input()
+    # delete monitoring window
+    dv.delete_monitoring_window()
+    # release usb camera
+    dv.release_camera()
+    # disconnect FT232H
+    ftdi.close()
+
+
+if __name__ == '__main__':
+    main()
