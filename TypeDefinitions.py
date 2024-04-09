83a2c2d4d2a88681a3f4978d730897acc8fc8adb
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-04-10 01:11:25 +0200

    some refactoring

--- TypeDefinitions.py
+++ TypeDefinitions.py
@@ -3,4 +3,3 @@ from typing import TypedDict
 import numpy.typing as npt
 
 StateType = TypedDict('StateType', {'img': npt.NDArray, 'img_count': int})
-ImgDescType = TypedDict('ImgDescType', {'number_of_data_bytes': int, 'img_count': int})
