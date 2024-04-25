8bb9eeea7408f8890eb037389c51896522457215
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-04-26 00:50:32 +0200

    import corrrected

--- TypeDefinitions.py
+++ TypeDefinitions.py
@@ -1,6 +1,8 @@
+from multiprocessing.pool import AsyncResult
+from multiprocessing.shared_memory import SharedMemory
+
 import numpy.typing as npt
-from mypy.typeshed.stdlib.multiprocessing.pool import AsyncResult
-from mypy.typeshed.stdlib.multiprocessing.shared_memory import SharedMemory
+
 
 StateType = tuple[npt.NDArray, int]
 ImgDescType = tuple[int, int]
