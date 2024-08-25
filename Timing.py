1446d3d3099bdd060800500cd1f01f262c2fbe20
Juergen Pfundt <juergen.pfundt@gmail.com>
2024-08-25 20:57:14 +0200

    removed Adafruit libraries

--- Timing.py
+++ Timing.py
@@ -1,10 +1,12 @@
 timing: [{
     "count": int,
     "cycle": float,
+    "intro": float,
     "work": float,
     "read": float,
     "latency": float,
     "delta": float,
     "wait_time": float,
+    "total_work": float,
     "total": float
 }] = []
