2814f9585c5996f04e7889a9884df99120104e66
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-02-24 12:18:55 +0100

    build standalone executable

---
+++ digitize.bat
@@ -0,0 +1,9 @@
+
+@echo on
+rem This script was created by Nuitka to execute 'beck-view-digitize.exe' with Python DLL being found.
+set PATH="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313";%PATH%
+set PYTHONHOME="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313"
+cd "%~dp0"
+call venv\Scripts\activate.bat
+python main.pyx %*
+deactivate
---
+++ digitize.cmd
@@ -0,0 +1,8 @@
+@echo off
+rem This script is used to execute 'beck-view-digitize' with Python DLL being found.
+set PATH="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313";%PATH%
+set PYTHONHOME="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313"
+cd "%~dp0"
+call venv\Scripts\activate.bat
+python main.pyx %*
+deactivate
