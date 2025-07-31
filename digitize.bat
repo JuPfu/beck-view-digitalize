1d5646c27a12ed9b70ca03f097694c10d7cf6b5b
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-07-31 12:56:07 +0200

    Windows specific start script for beck-view-digitize modified

--- digitize.bat
+++ digitize.bat
@@ -1,9 +1,8 @@
-
 @echo on
 rem This script was created by Nuitka to execute 'beck-view-digitize.exe' with Python DLL being found.
 set PATH="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313";%PATH%
 set PYTHONHOME="C:\Users\Peter lokal\AppData\Local\Programs\Python\Python313"
 cd "%~dp0"
 call venv\Scripts\activate.bat
-python main.pyx %*
+beck-view-digitize %*
 deactivate
