17c29af9e6288a93384cea03b4fc91d879f619b2
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-08-03 13:25:30 +0200

    reconfigured the build process. Windows has still to be tested on Geralds machine.

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ mv *.so dist/
 pyinstaller beck-view-digitize.spec --noconfirm
 mv dist/beck-view-digitize .
 chmod +x ./beck-view-digitize
+dir=$(pwd -P)
+echo 'Executable `beck-view-digitize` ready for usage in directory' $dir
