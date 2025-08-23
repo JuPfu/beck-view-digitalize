fd631caa8fb3929664beede0a6f4576d43253188
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-08-23 22:35:26 +0200

    correction of batch commands

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
-rmdir build
-rmdir dist
-del *.c *.so
+rmdir /S /O build
+rmdir /S /O dist
+del *.c *.pyd
 python setup.py build_ext --inplace
 mkdir dist
-move *.so dist
+move *.pyd dist
 pyinstaller beck-view-digitize.spec --noconfirm
-move /y dist\beck-view-digitize.exe %~pi..
-echo "Executable `beck-view-digitize.exe` ready for usage in directory $(%~pi)"
+move /y dist\beck-view-digitize.exe ..
+echo "Executable `beck-view-digitize.exe` ready for usage in directory %CD%"
