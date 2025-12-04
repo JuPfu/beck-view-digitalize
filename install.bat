4d8191f6f52117ba75f7f682c8e56fec148bacb8
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-12-04 22:06:32 +0100

    modified install.bat

--- install.bat
+++ install.bat
@@ -1,10 +1,18 @@
+cd /d %~dp0
+
 rmdir /S /Q dist
 del *.c *.pyd
+
 python3 -m pip install --upgrade pip setuptools wheel cython
+
 python3 setup.py clean --all
 python3 setup.py build_ext --inplace
+
 mkdir dist
 move *.pyd dist
+
 pyinstaller beck-view-digitize.spec --noconfirm
+
 copy /y dist\beck-view-digitize-bundle\beck-view-digitize.exe "%CD%"
-echo "Executable `beck-view-digitize.exe` ready for usage in directory %CD%"
+
+echo Executable beck-view-digitize.exe ready for usage in directory %CD%
