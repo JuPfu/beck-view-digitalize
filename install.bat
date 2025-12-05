afed270750da65f7c03996bef038968d132cbf5f
Juergen Pfundt <juergen.pfundt@gmail.com>
2025-12-05 01:05:16 +0100

    copy dlls into the right location

--- install.bat
+++ install.bat
@@ -13,6 +13,13 @@ move *.pyd dist
 
 pyinstaller beck-view-digitize.spec --noconfirm
 
+echo Copying libpng and zlib runtime DLLs...
+
+set TRIPLET=x64-windows
+
+copy "%VCPKG_ROOT%\installed\%TRIPLET%\bin\libpng16.dll" dist\beck-view-digitize-bundle\ >nul
+copy "%VCPKG_ROOT%\installed\%TRIPLET%\bin\zlib1.dll" dist\beck-view-digitize-bundle\ >nul
+
 copy /y dist\beck-view-digitize-bundle\beck-view-digitize.exe "%CD%"
 
 echo Executable beck-view-digitize.exe ready for usage in directory %CD%
